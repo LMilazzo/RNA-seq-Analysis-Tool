1473b963ebd31900b9d3a15e33753999866ef5d1
LMilazzo <lucas.milazzo1311@gmail.com>
2025-07-31 13:39:34 -0400

    Documentation Added

--- Version 2.0.0/App/modules/HelpTabUI.R
+++ Version 2.0.0/App/modules/HelpTabUI.R
@@ -10,7 +10,15 @@ helpTab <- function(){
         id = "Help Pages",
         
         tabPanel(
-          title = "File Usage Docs",
+          title = "File Usage/Types/Requirments",
+          includeHTML("Pages-Themes-Assets/HTML Pages/FileUsageDocumentation.HTML"),
+          style = "margin-top: 10px; padding-bottom: 25; padding: 0px; z-index: 1;"
+        ),
+        
+        tabPanel(
+          title = "Saving Files",
+          includeHTML("Pages-Themes-Assets/HTML Pages/SavingFilesDocumentation.HTML"),
+          style = "margin-top: 10px; padding-bottom: 25; padding: 0px; z-index: 1;"
         )
         
       )
